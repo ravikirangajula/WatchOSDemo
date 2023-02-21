6bafb55d9f6211602543c886ca4abcb188c686b8
Ravikiran <Ravikiran.gajula@lifefitness.com>
2023-02-21 23:27:46 +0800

    updated files

--- LFWatchApp/LFWatchApp/ViewController.swift
+++ LFWatchApp/LFWatchApp/ViewController.swift
@@ -19,33 +19,15 @@ class ViewController: UIViewController {
 
     
     @IBAction func tapOnButton(_ sender: Any) {
-        sharedObj.send("from ios") { outPutString in
-            print("Message: \(outPutString)")
+        sharedObj.handler = { [weak self] obj in
+            self?.titleLabel.text = obj
+        }
+        sharedObj.send("iOS App") { [weak self] outPutString in
+            DispatchQueue.main.async {
+                self?.titleLabel.text = outPutString
+            }
         }
     }
 }
 
-extension ViewController: WCSessionDelegate {
-    func sessionDidBecomeInactive(_ session: WCSession) {
-        print("sessionDidBecomeInactive")
-    }
-    
-    func sessionDidDeactivate(_ session: WCSession) {
-        print("sessionDidDeactivate")
 
-    }
-    
-    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
-        DispatchQueue.main.async {
-            self.titleLabel.text = "\(error?.localizedDescription)"
-        }
-        print("Error == \(error) \(activationState.rawValue)")
-    }
-    
-    func session(_ session: WCSession, didReceiveMessage message: [String : Any], replyHandler: @escaping ([String : Any]) -> Void) {
-        DispatchQueue.main.async {
-            self.titleLabel.text = message["message"] as? String ?? "App"
-        }
-        print("message == \(message)")
-    }
-}
